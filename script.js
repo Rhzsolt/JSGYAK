4bcb9823404df6f10d4faa21e3089244d562d1ec
Programozas Karrier <info@programozaskarrier.hu>
2021-10-05 18:05:51 +0200

    gyak

---
+++ script.js
@@ -0,0 +1,41 @@
+// Belső állapot
+let state = {
+  circle: 0,
+  square: 0,
+  rectangle: 0,
+};
+
+
+
+// 1. Selecteld ki id alapján a form elementet és submit eseményre 
+// köss be funkcionalitást
+
+document.getElementById("shapes").onsubmit = function(event){
+
+  event.preventDefault();
+
+  var x = event.target.elements.
+  console.log(x)
+
+ var plus = document.getElementsByClassName('fa fa-plus');
+
+
+
+ var minus= document.getElementsByClassName('fa fa-minus');
+
+
+
+
+
+  
+
+ }
+
+  
+
+
+// 2. Az esemény bekövetkezésekor olvasd ki az input mezőkben lévő adatokat
+
+// 3. Az adatok alapján módosítsd az alkalmazás belső állapotát
+
+// 4. A belső állapot alapján rajzold újra a formákat
